90befe5b0684134875618885be7f1fed3a31fffe
mulili <muli9611@gmail.com>
2021-09-12 08:19:09 +0800

    feat(App): add time

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ time }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      time: this.getTime(),
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.refresh();
+    }, 100);
+  },
+  methods: {
+    getTime() {
+      return new Date().toLocaleString();
+    },
+    refresh() {
+      this.time = this.getTime();
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
